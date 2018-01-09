f45388b949c527b70ac3fcd8dbf404ab64674e7b
StefanNienhuis <stefan.nienhuis@icloud.com>
2018-01-09 19:53:27 +0100

    Deprecated

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var request = require('request');
 var Triggi = require('node-triggi');
 var Service, Characteristic, HomebridgeAPI;
 
@@ -12,6 +11,7 @@ module.exports = function(homebridge) {
 };
 
 function TriggiPlatform(log, config) {
+  log.error('Due to the rename of Triggi, this package was replaced by homebridge-olisto. Install it with: npm install -g homebridge-olisto. This package will no longer work.')
   this.log = log;
   this.triggs = config.triggs;
 }
@@ -28,80 +28,5 @@ TriggiPlatform.prototype.accessories = function(callback) {
 }
 
 function TriggiAccessory(log, config, platform) {
-  this.platform = platform;
-  this.log = log;
-  this.name = config.name;
-  this.type = config.type || "push";
-  this.connecturl = config.connecturl
-  this.connector = new Triggi(this.connecturl);
 
-  // Check for valid config
-
-  if (this.type != "push" && this.type != "switch" && this.type != "outlet") {
-    log.error('Invalid type, please use "push", "switch" or "outlet"!');
-    log.error('Now using default "push."')
-    this.type = "push"
-  }
-
-  if (!this.connecturl.startsWith("https://connect.triggi.com/c/")) {
-    log.error('There is something wrong with your connect on URL! Make sure your URL starts with "https://"')
-  }
-
-  if (this.type == "push" || this.type == "switch") {
-    this.service = new Service.Switch(this.name);
-
-    this.service
-      .getCharacteristic(Characteristic.On)
-      //.on('get', this.getOn.bind(this))
-      .on('set', this.setOn.bind(this));
-  } else if (this.type == "outlet") {
-    this.service = new Service.Outlet(this.name)
-
-    this.service
-      .getCharacteristic(Characteristic.On)
-      //.on('get', this.getOn.bind(this))
-      .on('set', this.setOn.bind(this));
-
-    this.service
-      .setCharacteristic(Characteristic.OutletInUse, true)
-  }
-}
-
-TriggiAccessory.prototype.getInformationService = function() {
-  var informationService = new Service.AccessoryInformation();
-  informationService
-    .setCharacteristic(Characteristic.Name, this.name)
-    .setCharacteristic(Characteristic.Manufacturer, 'StefanNienhuis')
-    .setCharacteristic(Characteristic.Model, this.type.charAt(0).toUpperCase() + this.type.slice(1))
-    .setCharacteristic(Characteristic.SerialNumber, this.connecturl.replace("https://connect.triggi.com/c/", ""));
-
-  return informationService;
-}
-
-TriggiAccessory.prototype.getServices = function() {
-  return [this.service, this.getInformationService()];
-};
-
-/* Requested feature: get value of connector from Triggi
-TriggiAccessory.prototype.getOn = function(callback) {
-
-}
-*/
-
-TriggiAccessory.prototype.setOn = function(on, callback) {
-  var accessory = this;
-
-  if (accessory.type == "push") {
-    // Accessory is a push button
-    if (+on == 1) {
-      accessory.connector.setOn();
-      callback(accessory.service.setCharacteristic(Characteristic.On, 0));
-    }
-
-  } else if (accessory.type == "switch" || accessory.type == "outlet") {
-    // Accessory is an switch or outlet
-
-    accessory.connector.setValue('' + +on);
-    callback(null);
-  }
 }
